38b295af85c117ce58acedb9d1e3b25fde0d2b84
Vlad Ioffe <vlio20@gmail.com>
2015-10-08 20:10:39 +0300

    first comit - empty project

---
+++ src/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularMaterialFormBuilder')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
