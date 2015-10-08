de924a71957600c25caf703be64aa5f1c1d0f43c
Vlad Ioffe <vlio20@gmail.com>
2015-10-08 20:21:19 +0300

    cleaning repo

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,12 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController() {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1444323825140;
     vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -5,11 +5,8 @@
 
     beforeEach(module('angularMaterialFormBuilder'));
 
-    it('should define more than 5 awesome things', inject(function($controller) {
-      var vm = $controller('MainController');
-
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length > 5).toBeTruthy();
+    it('should check true to be true', inject(function($controller) {
+      expect(true).toBeTruthy();
     }));
   });
 })();
