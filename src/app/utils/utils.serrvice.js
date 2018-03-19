370349aff52bb9203a51f513c676c604f3032db0
asaf cohen <asafc@sgc.co.il>
2018-03-19 17:25:11 +0200

    support version 1.6.9

--- src/app/utils/utils.serrvice.js
+++ src/app/utils/utils.serrvice.js
@@ -4,13 +4,16 @@
   angular.module('angularMaterialFormBuilder')
     .service('Utils', Utils);
 
-  function Utils() {}
+  function Utils() { }
 
-  Utils.prototype.extend = function(dest, src) {
-    Object.keys(src).forEach(function(key) {
-      if(!dest.hasOwnProperty(key)) {
+  Utils.prototype.extend = function (dest, src) {
+    if (typeof dest === 'undefined') {
+      dest = {};
+    }
+    Object.keys(src).forEach(function (key) {
+      if (!dest.hasOwnProperty(key)) {
         dest[key] = src[key];
-      } else if(typeof src[key] === 'object') {
+      } else if (typeof src[key] === 'object') {
         this.extend(dest[key], src[key]);
       }
     }.bind(this));
