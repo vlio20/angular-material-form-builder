f28323971cd0fc6109f85695360d0b47bec3c869
Vito Macchia <vito.macchia@gmail.com>
2020-12-15 17:20:04 +0100

    prettified files

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,12 +1,11 @@
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
 
   describe('controllers', function () {
-
-    beforeEach(module('angularMaterialFormBuilder'));
+    beforeEach(module('angularMaterialFormBuilder'))
 
     it('should check true to be true', inject(function ($controller) {
-      expect(true).toBeTruthy();
-    }));
-  });
-})();
+      expect(true).toBeTruthy()
+    }))
+  })
+})()
