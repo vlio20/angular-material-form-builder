87b205c6f716c028327457d930a83db09934d22b
marco.messina <m.messina@bytesrl.com>
2021-03-16 16:55:09 +0100

    fix upload directive

--- src/lib/directives/upload-item/upload-item.controller.js
+++ src/lib/directives/upload-item/upload-item.controller.js
@@ -1,17 +1,17 @@
-/**
- * @ngInject
- *
- * @param {import('../../utils/utils.service').Utils} Utils
- * @param {ng.IScope} $scope
- * @param {JQLite} $element
- */
-function UploadItemCtrl($scope, Utils, $element) {
-  this.Scope = $scope
-  this.Element = $element
-  this.item = Utils.extend(this.item || {}, {
-    config: {},
-    options: [],
-  })
+class UploadItemCtrl {
+  /**
+   * @ngInject
+   *
+   * @param {import('../../utils/utils.service').Utils} Utils
+   * @param {JQLite} $element
+   */
+  constructor(Utils, $element) {
+    this.Element = $element
+    this.item = Utils.extend(this.item || {}, {
+      config: {},
+      options: [],
+    })
+  }
 }
 
 export { UploadItemCtrl }
